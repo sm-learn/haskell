4b1d18c527ee093993ff053246b08639469bbdb5
Suvankar Mazumder <smazumder@gmail.com>
2017-03-06 12:28:52 -0500

    Create haskell-install.sh

---
+++ scripts/haskell-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Bash provisioning base script for my laptop.
+#
+# The base script will install the following libs
+#
+# Kaskell Platform
+#  
+# Copyright@2014 smtechnocrat
+# Author: smtechnocrat
+# Send questions to smtechnocrat@gmail.com
+#
+################################################################## 
+# Install and Haskell Platform
+sudo apt-get install haskell-platform
+
